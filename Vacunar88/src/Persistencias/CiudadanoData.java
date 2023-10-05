cd20a257c59ade81a61eb1c4f026e07c23bcb796
FacundoBascur <facundodavidbascur@gmail.com>
2023-10-05 19:45:13 -0300

    metodos en ciudadano Data ABM-main

--- Vacunar88/src/Persistencias/CiudadanoData.java
+++ Vacunar88/src/Persistencias/CiudadanoData.java
@@ -1,28 +1,29 @@
-
 package Persistencias;
 
 import Entidades.Ciudadano;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
-
 public class CiudadanoData {
-    
-    private Connection con=null;
-    
-    public CiudadanoData(){
-    con=Conexion.getConexion();
-    }
-    
-public void registrarCiudadano(Ciudadano ciu){
 
-String sql= "INSERT INTO ciudadano (dni, nombreCompleto, email, celular, longXciu, latYciu, patologia, ambitoTrabajo) "
-        + "VALUES (?,?,?,?,?,?,?,?)";
+    private Connection con = null;
+
+    public CiudadanoData() {
+        con = Conexion.getConexion();
+    }
+
+    public void registrarCiudadano(Ciudadano ciu) {
+
+        String sql = "INSERT INTO ciudadano (dni, nombreCompleto, email, celular, longXciu, latYciu, patologia, ambitoTrabajo,dosis,estado) "
+                + "VALUES (?,?,?,?,?,?,?,?,?,?)";
 
         try {
-            PreparedStatement ps=con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, ciu.getDni());
             ps.setString(2, ciu.getNombreCompleto());
             ps.setString(3, ciu.getEmail());
@@ -31,22 +32,96 @@ String sql= "INSERT INTO ciudadano (dni, nombreCompleto, email, celular, longXci
             ps.setInt(6, ciu.getLatYciu());
             ps.setString(7, ciu.getPatologia());
             ps.setString(8, ciu.getAmbitoTrabajo());
-            
-        ps.executeUpdate();
-            
-          JOptionPane.showMessageDialog(null, "Ciudadano registrado correctamente.");
-            
-       } catch (SQLException ex) {
-          JOptionPane.showMessageDialog(null, "Error al conectarse a la tabla ciudadano.");
+            ps.setInt(9, ciu.getDosis());
+            ps.setBoolean(10, ciu.isEstado());
+
+            ps.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Ciudadano registrado correctamente.");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la tabla ciudadano.");
+        }
+    }
+
+    public List<Ciudadano> listarCiudadanosRegistrados() {
+
+        Ciudadano ciudadano = null;
+        List<Ciudadano> lista = new ArrayList<>();
+
+        try {
+
+            String sql = " SELECT * FROM ciudadano";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                ciudadano = new Ciudadano();
+                ciudadano.setDni(rs.getInt("dni"));
+                ciudadano.setNombreCompleto(rs.getString("nombreCompleto"));
+                ciudadano.setEmail(rs.getString("email"));
+                ciudadano.setCelular(rs.getLong("celular"));
+                ciudadano.setLongXciu(rs.getInt("longXciu"));
+                ciudadano.setLatYciu(rs.getInt("latYciu"));
+                ciudadano.setPatologia(rs.getString("patologia"));
+                ciudadano.setAmbitoTrabajo(rs.getString("ambitoTrabajo"));
+                ciudadano.setDosis(rs.getInt("dosis"));
+                ciudadano.setEstado(rs.getBoolean("estado"));
+                lista.add(ciudadano);
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla ciudadanos.");
         }
 
+        return lista;
+    }
+    
+    public List<Ciudadano> buscarXdni(int dni){
+    
+        Ciudadano ciudadano = null;
+        List<Ciudadano> lista = new ArrayList<>();
 
+        try {
 
+            String sql = " SELECT * FROM ciudadano WHERE dni = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, dni);
+            ResultSet rs = ps.executeQuery();
 
-}
+            while (rs.next()) {
+
+                ciudadano = new Ciudadano();
+                ciudadano.setDni(dni);
+                ciudadano.setNombreCompleto(rs.getString("nombreCompleto"));
+                ciudadano.setEmail(rs.getString("email"));
+                ciudadano.setCelular(rs.getLong("celular"));
+                ciudadano.setLongXciu(rs.getInt("longXciu"));
+                ciudadano.setLatYciu(rs.getInt("latYciu"));
+                ciudadano.setPatologia(rs.getString("patologia"));
+                ciudadano.setAmbitoTrabajo(rs.getString("ambitoTrabajo"));
+                ciudadano.setDosis(rs.getInt("dosis"));
+                ciudadano.setEstado(rs.getBoolean("estado"));
+                lista.add(ciudadano);
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla ciudadanos.");
+        }
+
+        return lista;
     
     
     
+    }
+
     
-    
+
 }
--- Vacunar88/src/Vistas/Main.java
+++ Vacunar88/src/Vistas/Main.java
@@ -11,10 +11,16 @@ public class Main {
    
     public static void main(String[] args) {
    
-   Ciudadano ciu=new Ciudadano(32608741, "Lucas Monzon", "lucasmonzon@gmail.com", 2214856824l, 5, 9, "Diabetes", "Educación");
+   //Ciudadano ciu=new Ciudadano(32608741, "Lucas Monzon", "lucasmonzon@gmail.com", 2214856824l, 5, 9, "Diabetes", "Educación",0,true);
+   //Ciudadano ciu3=new Ciudadano(32608740, "Este Banquito", "estebanquito@gmail.com", 221412324l, 5, 9, null, "Ingeniero",0,true);
       
    CiudadanoData ciu2=new CiudadanoData();
-   ciu2.registrarCiudadano(ciu);
+   //ciu2.registrarCiudadano(ciu);
+   //ciu2.registrarCiudadano(ciu3);
+   
+        //System.out.println(ciu2.listarCiudadanosRegistrados());
+        System.out.println(ciu2.buscarXdni(32608741));
+   
                 }
     
 }
