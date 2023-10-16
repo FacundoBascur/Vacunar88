f890fdc5a9ae402dd07688e0a3151322e5379a08
SabriBardiera <sabrinabardiera@hotmail.com>
2023-10-16 00:37:58 -0300

    Vista y clase busq centro, falta terminar

--- Vacunar88/src/Persistencias/CentroVacunacionData.java
+++ Vacunar88/src/Persistencias/CentroVacunacionData.java
@@ -1,4 +1,3 @@
-
 package Persistencias;
 
 import Entidades.CentroVacunacion;
@@ -8,38 +7,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
-import Entidades.CitaVacunacion;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class CentroVacunacionData {
-    
-      private Connection con = null;
-    
+
+    private Connection con = null;
+
     public CentroVacunacionData() {
-    con = Conexion.getConexion();
-    } 
-    
-     public void registrarCentro(CentroVacunacion centro) {
+        con = Conexion.getConexion();
+    }
+
+    public void registrarCentro(CentroVacunacion centro) {
 
         String sql = "INSERT INTO centrovacunacion (nombre,longXcentro,latYcentro,estado) VALUES (?,?,?,?)";
 
-         try {
+        try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            
             ps.setString(1, centro.getNombre());
             ps.setInt(2, centro.getLongXcentro());
-           ps.setInt(3, centro.getLatYcentro());
-          ps.setBoolean(4, centro.isEstado());
+            ps.setInt(3, centro.getLatYcentro());
+            ps.setBoolean(4, centro.isEstado());
             ps.executeUpdate();
 
             ResultSet rs = ps.getGeneratedKeys();
 
             if (rs.next()) {
 
-               centro.setCodCentro(rs.getInt(1));
+                centro.setCodCentro(rs.getInt(1));
                 JOptionPane.showMessageDialog(null, "Centro Vacunatorio registrado exitosamente.");
                 ps.close();
 
@@ -51,9 +47,9 @@ public class CentroVacunacionData {
             JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunación.");
         }
 
-    }    
-    
-     public List<CentroVacunacion> listarCentros() {
+    }
+
+    public List<CentroVacunacion> listarCentros() {
         CentroVacunacion centro = null;
         List<CentroVacunacion> lista = new ArrayList();
 
@@ -70,7 +66,7 @@ public class CentroVacunacionData {
                 centro.setNombre(rs.getString("nombre"));
                 centro.setLongXcentro(rs.getInt("longXcentro"));
                 centro.setLatYcentro(rs.getInt("latYcentro"));
-               centro.setEstado(rs.getBoolean("estado"));
+                centro.setEstado(rs.getBoolean("estado"));
                 lista.add(centro);
 
             }
@@ -83,6 +79,181 @@ public class CentroVacunacionData {
         return lista;
 
     }
-    
-    
+
+    public List<CentroVacunacion> listarPorEstado(boolean estado) {
+        CentroVacunacion centro = null;
+        List<CentroVacunacion> lista = new ArrayList();
+
+        if (estado == false) {
+            String sql = "SELECT * FROM centrovacunacion WHERE estado=0";
+
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setBoolean(1, estado);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+
+                    centro = new CentroVacunacion();
+                    centro.setCodCentro(rs.getInt("codCentro"));
+                    centro.setNombre(rs.getString("nombre"));
+                    centro.setLongXcentro(rs.getInt("longXcentro"));
+                    centro.setLatYcentro(rs.getInt("latYcentro"));
+                    centro.setEstado(rs.getBoolean("estado"));
+                    lista.add(centro);
+                }
+                ps.close();
+
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al acceder a alumnos");
+            }
+            return lista;
+
+        } else {
+
+            String sql = "SELECT * FROM centrovacunacion WHERE estado=1";
+
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setBoolean(1, estado);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+
+                    centro = new CentroVacunacion();
+                    centro.setCodCentro(rs.getInt("codCentro"));
+                    centro.setNombre(rs.getString("nombre"));
+                    centro.setLongXcentro(rs.getInt("longXcentro"));
+                    centro.setLatYcentro(rs.getInt("latYcentro"));
+                    centro.setEstado(rs.getBoolean("estado"));
+                    lista.add(centro);
+ }
+
+                ps.close();
+
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al acceder a alumnos");
+            }
+            return lista;
+        }
+    }
+
+    public CentroVacunacion buscarPorCodCentro(int cod) {
+
+        CentroVacunacion centro = null;
+        String sql = "SELECT * FROM centrovacunacion WHERE codCentro = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                centro = new CentroVacunacion();
+                centro.setCodCentro(rs.getInt("codCentro"));
+                centro.setNombre(rs.getString("nombre"));
+                centro.setLongXcentro(rs.getInt("longXcentro"));
+                centro.setLatYcentro(rs.getInt("latYcentro"));
+                centro.setEstado(rs.getBoolean("estado"));
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunacion.");
+        }
+
+        return centro;
+
+    }
+
+    public CentroVacunacion buscarPorNombre(String nom) {
+
+        CentroVacunacion centro = null;
+        String sql = "SELECT * FROM centrovacunacion WHERE nombre = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nom);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                centro = new CentroVacunacion();
+                centro.setCodCentro(rs.getInt("codCentro"));
+                centro.setNombre(rs.getString("nombre"));
+                centro.setLongXcentro(rs.getInt("longXcentro"));
+                centro.setLatYcentro(rs.getInt("latYcentro"));
+                centro.setEstado(rs.getBoolean("estado"));
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunacion.");
+        }
+
+        return centro;
+
+    }
+  public void modificarCentroVacunacion(int cod, String nom, int longi, int lati, boolean est) {
+
+        CentroVacunacion centro = null;
+        String sql = "UPDATE centrovacunacion SET codCentro=?, nombre=? , longXcentro=? , latYcentro=? WHERE codCentro=? ";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            ps.setString(2, nom);
+            ps.setInt(3, longi);
+            ps.setInt(4, lati);
+            ps.setBoolean(5, est);
+            int result = ps.executeUpdate();
+
+            if (result == 1) {
+                JOptionPane.showMessageDialog(null, "Centro de vacunación modificado con éxito.");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunación.");
+        }
+
+    }
+   public void darDeAlta(int cod) {
+
+        String sql = "UPDATE centrovacunacion SET estado= 1 WHERE codCentro=?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            int result = ps.executeUpdate();
+
+            if (result == 1) {
+                JOptionPane.showMessageDialog(null, "Centro de Vacunación dado de alta exitosamente.");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunación.");
+        }
+
+    }
+public void darDeBaja(int cod) {
+
+        String sql = "UPDATE centrovacunacion SET estado= 0 WHERE codCentro=?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            int result = ps.executeUpdate();
+
+            if (result == 1) {
+                JOptionPane.showMessageDialog(null, "Centro de Vacunación dado de baja exitosamente.");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a CentroVacunación.");
+        }
+
+    }
 }
--- Vacunar88/src/Vistas/BusqModifCentros.java
+++ Vacunar88/src/Vistas/BusqModifCentros.java
@@ -1,18 +1,18 @@
-
 package Vistas;
 
+import java.sql.Date;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-
 public class BusqModifCentros extends javax.swing.JInternalFrame {
-DefaultTableModel tabla = new DefaultTableModel();
-   
+
+    DefaultTableModel tabla = new DefaultTableModel();
+
     public BusqModifCentros() {
         initComponents();
         armarTabla();
     }
 
-   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -29,7 +29,7 @@ DefaultTableModel tabla = new DefaultTableModel();
 
         setTitle("                                     Búsqueda y Modificación de Centros Vacunatorios");
 
-        jCOpciones.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "<Seleccionar>", "Código ", "Nombre", "Activos", "Inactivos", "Todos", " " }));
+        jCOpciones.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "<Seleccionar>", "Código", "Nombre", "Activos", "Inactivos", "Todos" }));
         jCOpciones.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jCOpcionesActionPerformed(evt);
@@ -43,6 +43,11 @@ DefaultTableModel tabla = new DefaultTableModel();
             }
         });
 
+        tablaAlumnos = new javax.swing.JTable(){
+            public boolean isCellEditable(int rowIndex, int columnIndex){
+                return columnIndex > 0 && columnIndex < 5;
+            }
+        };
         jTTablaCentros.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -54,6 +59,7 @@ DefaultTableModel tabla = new DefaultTableModel();
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTTablaCentros.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(jTTablaCentros);
 
         jBModificar.setText("Modificar");
@@ -138,35 +144,75 @@ DefaultTableModel tabla = new DefaultTableModel();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jCOpcionesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCOpcionesActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jCOpcionesActionPerformed
 
     private void jBBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBBuscarActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jBBuscarActionPerformed
 
     private void jBModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBModificarActionPerformed
-        // TODO add your handling code here:
+        try {
+            if (jTTablaCentros.getSelectedRow() == -1) {
+                JOptionPane.showMessageDialog(null, "No se ha realizado ninguna modificacion");
+            } else {
+
+                //En estos variables se guardan los datos obtenidos al seleccionar una fila de la tabla. "tabla.getValueAt(tablaAlumnos.getSelectedRow --> selecciona fila"
+                int cod = Integer.parseInt(jTTablaCentros.getValueAt(jTTablaCentros.getSelectedRow(), 0).toString());
+                String nom = jTTablaCentros.getValueAt(jTTablaCentros.getSelectedRow(), 1).toString();
+                int longXcentro = Integer.parseInt(jTTablaCentros.getValueAt(jTTablaCentros.getSelectedRow(), 2).toString());
+                int lat = Integer.parseInt(jTTablaCentros.getValueAt(jTTablaCentros.getSelectedRow(), 3).toString());
+                String est = jTTablaCentros.getValueAt(jTTablaCentros.getSelectedRow(), 4).toString();
+
+                if (nom.isEmpty()||  ) {
+                    JOptionPane.showMessageDialog(null, "No se admiten numeros y/o casilleros vacios en Apellido y Nombre");
+                    buscarActionPerformed(evt);
+                } else {
+
+                    String[] list = {"Si", "No"};
+                    int opcion = JOptionPane.showOptionDialog(null, " Confirmar modificacion", "", 0, JOptionPane.QUESTION_MESSAGE, null, list, ""); // muestra un cuadro de dialogo para confirmar la modificacion del alumno devuelve 0 o 1 dependiendo la opcion que se elija
+
+                    if (opcion == 0) {
+                        alumno.modificarAlumno(id, dni, ap, nom, fecha);
+
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Modificacion cancelada");
+                    }
+
+                }
+            }
+        } catch (NumberFormatException | NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Error en la modificacion - el DNI colocado no es correcto, excede limites de digitos");
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, "Error en el formato de fecha");
+
+        }
+
+
     }//GEN-LAST:event_jBModificarActionPerformed
 
     private void jBAltaBajaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAltaBajaActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jBAltaBajaActionPerformed
 
     private void jBSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBSalirActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jBSalirActionPerformed
 
- private void armarTabla() {
-        String[] titulos = new String[]{"Código","Nombre", "Longitud", "Latitud", "Estado"};
+    private void armarTabla() {
+        String[] titulos = new String[]{"Código", "Nombre", "Longitud", "Latitud", "Estado"};
         tabla.setColumnIdentifiers(titulos);
         jTTablaCentros.setModel(tabla);
-    
-    
-    
- }
-    
-    
+    }
+
+    public boolean verificarNombre(String cadena2) {
+        try {
+            Integer.parseInt(cadena2);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBAltaBaja;
     private javax.swing.JButton jBBuscar;
