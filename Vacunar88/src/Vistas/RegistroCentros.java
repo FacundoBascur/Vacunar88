6f0e3e1fe1a6ce2ea131a4bcae8618dd921f647f
SabriBardiera <sabrinabardiera@hotmail.com>
2023-10-20 00:56:09 -0300

    centro, falta el modif

--- Vacunar88/src/Vistas/RegistroCentros.java
+++ Vacunar88/src/Vistas/RegistroCentros.java
@@ -1,43 +1,36 @@
-
 package Vistas;
 
 import Entidades.CentroVacunacion;
 import Persistencias.CentroVacunacionData;
 import javax.swing.JOptionPane;
 
-
 public class RegistroCentros extends javax.swing.JInternalFrame {
- CentroVacunacionData centro = new CentroVacunacionData();
-   
+
+    CentroVacunacionData centro = new CentroVacunacionData();
+
     public RegistroCentros() {
         initComponents();
     }
 
-   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jTNombre = new javax.swing.JTextField();
-        jTLong = new javax.swing.JTextField();
-        jTLat = new javax.swing.JTextField();
+        jTZona = new javax.swing.JTextField();
         jCActivo = new javax.swing.JCheckBox();
         jBSalir = new javax.swing.JButton();
         jBGuardar = new javax.swing.JButton();
-        jBNuevo = new javax.swing.JButton();
 
-        setTitle("                                          Registro de Centros de Vacunación");
+        setTitle("        Registro de Centros de Vacunación");
 
         jLabel1.setText("Nombre");
 
-        jLabel2.setText("Longitud");
-
-        jLabel3.setText("Latitud");
+        jLabel3.setText("Zona");
 
         jLabel4.setText("Estado");
 
@@ -57,71 +50,53 @@ public class RegistroCentros extends javax.swing.JInternalFrame {
             }
         });
 
-        jBNuevo.setText("Nuevo");
-        jBNuevo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBNuevoActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addComponent(jBNuevo)
-                        .addGap(114, 114, 114)
-                        .addComponent(jBGuardar))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(85, 85, 85)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(75, 75, 75)
-                                .addComponent(jLabel4)))
-                        .addGap(42, 42, 42)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jCActivo)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jTNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
-                                .addComponent(jTLong)
-                                .addComponent(jTLat)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(218, Short.MAX_VALUE)
+                .addComponent(jBGuardar)
+                .addGap(136, 136, 136)
                 .addComponent(jBSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36))
+                .addGap(17, 17, 17))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(46, 46, 46)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jCActivo)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jTNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
+                        .addComponent(jTZona)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(54, 54, 54)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jTLong, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(52, 52, 52)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jTLat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(68, 68, 68)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jCActivo))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBSalir)
-                    .addComponent(jBGuardar)
-                    .addComponent(jBNuevo))
-                .addGap(29, 29, 29))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addGap(29, 29, 29)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTZona, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(32, 32, 32)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jCActivo)
+                            .addComponent(jLabel4)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(199, 199, 199)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jBGuardar)
+                            .addComponent(jBSalir))))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -139,74 +114,52 @@ public class RegistroCentros extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBSalirActionPerformed
-     this.dispose();
+        this.dispose();
     }//GEN-LAST:event_jBSalirActionPerformed
 
-    private void jBNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBNuevoActionPerformed
-         if (jTNombre != null && jTLong != null && jTLat != null) {
-
-            jTNombre.setText("");
-            jTLong.setText("");
-            jTLat.setText("");
-            jCActivo.setSelected(false);
-
-        }
-    }//GEN-LAST:event_jBNuevoActionPerformed
-
     private void jBGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardarActionPerformed
-        
 
         try {
-            if (jTNombre.getText().isEmpty() || jTLong.getText().isEmpty() || jTLat.getText().isEmpty()) {
+            if (jTNombre.getText().isEmpty() || jTZona.getText().isEmpty()) {
                 JOptionPane.showMessageDialog(null, "No puede haber campos de texto vacíos.");
 
-            } else if ((jTLong.getText()).length() > 1 || (jTLong.getText()).length() < 1
-                || (jTLat.getText()).length() < 1 || (jTLat.getText()).length() > 1) {
-                JOptionPane.showMessageDialog(null, "Cantidad de dígitos fuera de rango permitido.");
-
-            } else if (verificar(jTNombre.getText()) == false) {
+            } else if (verificar(jTNombre.getText())==false) {
                 JOptionPane.showMessageDialog(null, "El campo Nombre no puede contener números.");
-
-            } else if (verificar(jTLong.getText()) == true || verificar(jTLat.getText()) == true) {
-                JOptionPane.showMessageDialog(null, "Los campos Longitud y Latitud no pueden contener letras.");
-
-            }  else{
-                    centro.registrarCentro(new CentroVacunacion(jTNombre.getText(), Integer.parseInt(jTLong.getText()),
-                        Integer.parseInt(jTLat.getText()), jCActivo.isSelected()));
-                
+                  jTNombre.setText("");
+            }else if(verificar(jTZona.getText())==false){
+                   JOptionPane.showMessageDialog(null, "El campo Zona no puede contener números.");
+                    jTZona.setText("");
+            } else {
+                centro.registrarCentro(new CentroVacunacion(jTNombre.getText(), jTZona.getText(), jCActivo.isSelected()));
 
                 jTNombre.setText("");
-                jTLong.setText("");
-                jTLat.setText("");
+                jTZona.setText("");
                 jCActivo.setSelected(false);
-            
+
             }
-        }catch (NumberFormatException | NullPointerException e) {
-        JOptionPane.showMessageDialog(null, "Error al registrar, campos en formato incorrecto.");
-        }            
-       
+        } catch (NumberFormatException | NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar.");
+        }
+
     }//GEN-LAST:event_jBGuardarActionPerformed
- public boolean verificar(String cadena) {
+    public boolean verificar(String cadena) {
         try {
             Integer.parseInt(cadena);
             return false;
         } catch (NumberFormatException e) {
             return true;
         }
- 
- }
+
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBGuardar;
-    private javax.swing.JButton jBNuevo;
     private javax.swing.JButton jBSalir;
     private javax.swing.JCheckBox jCActivo;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTLat;
-    private javax.swing.JTextField jTLong;
     private javax.swing.JTextField jTNombre;
+    private javax.swing.JTextField jTZona;
     // End of variables declaration//GEN-END:variables
 }
